2deaaaafaa083b26b0eef5fb4f6c4476b5082a93
Me <park.pavel04@gmail.com>
2023-10-06 03:46:23 +0600

    создание условия если общая сумма меньше нуля то вывод сообщения

--- src/Clothes.java
+++ src/Clothes.java
@@ -27,5 +27,8 @@ public class Clothes {
         if(totalAmount > 0){
             System.out.println("Общая сумма покупки составляет: tg" + totalAmount);
         }
+        else {
+            System.out.println("Корзина Пуста. Добавьте Товар перед оплатой");
+        }
     }
     }
