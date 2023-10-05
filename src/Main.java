28d3d9949737798f380351b71c41997f2ca314f8
Me <park.pavel04@gmail.com>
2023-10-06 03:24:55 +0600

    создание меню и считывание выбора пользователя

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@ public class Main {
         Payment paymentMethod = null;
 
         while (true){
+            System.out.println("ZARA");
+            System.out.println("1. Добавить товар в корзину");
+            System.out.println("2. Посмотреть корзину");
+            System.out.println("3. Выбрать метод оплаты");
+            System.out.println("4. Оплатить покупку");
+            System.out.println("5. Выход");
 
+            System.out.print("Выберите действие: ");
+            int choice = scanner.nextInt();
         }
     }
 }
