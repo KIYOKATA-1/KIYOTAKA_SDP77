577f477698dd0e5983b305b2fbbce15d18b6da9b
Me <park.pavel04@gmail.com>
2023-10-06 02:50:30 +0600

    Создание Конструктора класса Product, инициализация поля name и price при создании объекта Product

--- src/Product.java
+++ src/Product.java
@@ -2,5 +2,8 @@ public class Product {
     private String name;
     private double price;
 
-
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
 }
