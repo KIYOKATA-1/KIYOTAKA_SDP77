aa6b79d87eb49c1d3f55b5830a57c06e5e306fc7
Me <park.pavel04@gmail.com>
2023-10-06 03:17:33 +0600

    Реализация метода pay из интерфейса Payment

--- src/CreditCardPayment.java
+++ src/CreditCardPayment.java
@@ -1,3 +1,14 @@
 public class CreditCardPayment implements Payment{
+    private String Name;
+    private String cardHolderName;
 
-}
+    public CreditCardPayment(String cardHolderName, String cardNumber) {
+        this.cardHolderName = cardHolderName;
+        this.Name = cardNumber;
+        }
+
+        @Override
+    public void pay(double amount){
+            System.out.println("Оплачено " + amount + " с использованием кредитной карты, номер которой заканчивается на " + Name.substring(Name.length() - 4));
+        }
+    }
