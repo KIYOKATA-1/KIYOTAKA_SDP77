c5dcfb4ce5a74c53bb94338abbbf9f3924e9c3c4
Me <park.pavel04@gmail.com>
2023-10-06 02:45:55 +0600

    Создание конструктора класса Cart и инициализация списка products как новый Array List

--- src/Cart.java
+++ src/Cart.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 public class Cart {
     private List<Product> products;
+
+    public Cart() {
+        this.products = new ArrayList<>();
+    }
 }
